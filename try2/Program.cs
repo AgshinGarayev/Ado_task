3149edb329f5887004ce78d1994af0683a1394f4
AgshinGarayev <59489449+AgshinGarayev@users.noreply.github.com>
2023-09-25 12:19:17 +0400

    Update Program.cs

--- try2/Program.cs
+++ try2/Program.cs
@@ -12,44 +12,80 @@ namespace try2
         static void Main(string[] args)
         {
             string connectionString = "Host=localhost;Port=5432;Username=postgres;Password=Aqsin1213;Database=Mytask;";
-
-            Users newUser = new Users
-            {
-                user_id = 1,
-                name = "Test",
-                email = "somthing@something.com",
-                password = "password",
-                location = "Baku",
-                join_date = DateTime.Now,                
-            };
+            int userIdToUpdate = 2;
+            int userIdToDelete = 2; 
 
             using (NpgsqlConnection connection = new NpgsqlConnection(connectionString))
             {
                 connection.Open();
 
-                using (NpgsqlCommand cmd = new NpgsqlCommand())
+                using (NpgsqlTransaction transaction = connection.BeginTransaction())
                 {
-                    cmd.Connection = connection;
-
-                    cmd.CommandText = "INSERT INTO users (user_id, email, password, name, location, join_date) VALUES (@id, @email, @password, @name, @location, @join_date)";
-                    cmd.Parameters.AddWithValue("@id", newUser.user_id);
-                    cmd.Parameters.AddWithValue("@email", newUser.email);
-                    cmd.Parameters.AddWithValue("password", newUser.password);
-                    cmd.Parameters.AddWithValue("@name", newUser.name);
-                    cmd.Parameters.AddWithValue("@location", newUser.location);
-                    cmd.Parameters.AddWithValue("@join_date", newUser.join_date);
-
-                    int rowsAffected = cmd.ExecuteNonQuery();
-
-                    if (rowsAffected > 0)
+                    try
                     {
-                        Console.WriteLine("User added successfully!");
+                        InsertNewUser(connection, transaction, 7, "email@tryemail.com", "password123", "Username123", "mylocation");
+
+
+
+
+                        using (NpgsqlCommand updateCmd = new NpgsqlCommand())
+                        {
+                            updateCmd.Connection = connection;
+                            updateCmd.Transaction = transaction;
+
+                            updateCmd.CommandText = "UPDATE users SET name = @newName WHERE user_id = @userId";
+                            updateCmd.Parameters.AddWithValue("@newName", "UpdatedNameAgshin");
+                            updateCmd.Parameters.AddWithValue("@userId", userIdToUpdate);
+
+                            int updateRowsAffected = updateCmd.ExecuteNonQuery();
+                            Console.WriteLine($"Rows Updated: {updateRowsAffected}");
+                        }
+
+
+                        using (NpgsqlCommand deleteCmd = new NpgsqlCommand())
+                        {
+                            deleteCmd.Connection = connection;
+                            deleteCmd.Transaction = transaction;
+
+                            deleteCmd.CommandText = "DELETE FROM users WHERE user_id = @userId";
+                            deleteCmd.Parameters.AddWithValue("@userId", userIdToDelete);
+
+                            int deleteRowsAffected = deleteCmd.ExecuteNonQuery();
+                            Console.WriteLine($"Rows Deleted: {deleteRowsAffected}");
+                        }
+
+
+                        transaction.Commit();
                     }
-                    else
+                    catch (Exception ex)
                     {
-                        Console.WriteLine("Failed to add user.");
+                        // Handle errors and rollback the transaction
+                        transaction.Rollback();
+                        Console.WriteLine("Transaction Error: " + ex.Message);
                     }
                 }
+            }
+        }
+
+
+
+        static void InsertNewUser(NpgsqlConnection connection, NpgsqlTransaction transaction, int user_id, string email, string password, string name, string location)
+        {
+            using (NpgsqlCommand insertCmd = new NpgsqlCommand())
+            {
+                insertCmd.Connection = connection;
+                insertCmd.Transaction = transaction;
+
+                insertCmd.CommandText = "INSERT INTO users (user_id, email, password, name, location, join_date) VALUES (@user_id, @email, @password, @name, @location, @join_date)";
+                insertCmd.Parameters.AddWithValue("@user_id", user_id);
+                insertCmd.Parameters.AddWithValue("@email", email);
+                insertCmd.Parameters.AddWithValue("@password", password);
+                insertCmd.Parameters.AddWithValue("@name", name);
+                insertCmd.Parameters.AddWithValue("@location", location);
+                insertCmd.Parameters.AddWithValue("@join_date", DateTime.Now);
+
+                int insertRowsAffected = insertCmd.ExecuteNonQuery();
+                Console.WriteLine($"Rows Inserted: {insertRowsAffected}");
             }
         }
     }

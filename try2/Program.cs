bcdad694cb6e16e5cbab95c3e1fe7610d7c953f2
AgshinGarayev <59489449+AgshinGarayev@users.noreply.github.com>
2023-09-25 14:06:56 +0400

    Update Program.cs

--- try2/Program.cs
+++ try2/Program.cs
@@ -1,9 +1,5 @@
-﻿using Npgsql;
+using Npgsql;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace try2
 {
@@ -12,8 +8,9 @@ namespace try2
         static void Main(string[] args)
         {
             string connectionString = "Host=localhost;Port=5432;Username=postgres;Password=Aqsin1213;Database=Mytask;";
-            int userIdToUpdate = 2;
-            int userIdToDelete = 2; 
+            int userIdToSelect = 12;
+            int userIdToUpdate = 1; 
+            int userIdToDelete = 6; 
 
             using (NpgsqlConnection connection = new NpgsqlConnection(connectionString))
             {
@@ -23,53 +20,31 @@ namespace try2
                 {
                     try
                     {
-                        InsertNewUser(connection, transaction, 7, "email@tryemail.com", "password123", "Username123", "mylocation");
+                       
+                        SelectUser(connection, transaction, userIdToSelect);
 
+                        UpdateUserName(connection, transaction, userIdToUpdate, "UpdatedNameAgshin");
 
-
-
-                        using (NpgsqlCommand updateCmd = new NpgsqlCommand())
-                        {
-                            updateCmd.Connection = connection;
-                            updateCmd.Transaction = transaction;
-
-                            updateCmd.CommandText = "UPDATE users SET name = @newName WHERE user_id = @userId";
-                            updateCmd.Parameters.AddWithValue("@newName", "UpdatedNameAgshin");
-                            updateCmd.Parameters.AddWithValue("@userId", userIdToUpdate);
-
-                            int updateRowsAffected = updateCmd.ExecuteNonQuery();
-                            Console.WriteLine($"Rows Updated: {updateRowsAffected}");
-                        }
-
-
-                        using (NpgsqlCommand deleteCmd = new NpgsqlCommand())
-                        {
-                            deleteCmd.Connection = connection;
-                            deleteCmd.Transaction = transaction;
-
-                            deleteCmd.CommandText = "DELETE FROM users WHERE user_id = @userId";
-                            deleteCmd.Parameters.AddWithValue("@userId", userIdToDelete);
-
-                            int deleteRowsAffected = deleteCmd.ExecuteNonQuery();
-                            Console.WriteLine($"Rows Deleted: {deleteRowsAffected}");
-                        }
-
+                        DeleteUser(connection, transaction, userIdToDelete);
+                        InsertNewUser(connection, transaction, 12, "mmmm@mmm.com", "pass", "someht", "someh", DateTime.Now);
 
                         transaction.Commit();
                     }
                     catch (Exception ex)
                     {
-                        // Handle errors and rollback the transaction
+                       
                         transaction.Rollback();
                         Console.WriteLine("Transaction Error: " + ex.Message);
+                    }
+                    finally
+                    {
+                        connection.Close();
                     }
                 }
             }
         }
 
-
-
-        static void InsertNewUser(NpgsqlConnection connection, NpgsqlTransaction transaction, int user_id, string email, string password, string name, string location)
+        static void InsertNewUser(NpgsqlConnection connection, NpgsqlTransaction transaction, int user_id, string email, string password, string name, string location, DateTime join_date)
         {
             using (NpgsqlCommand insertCmd = new NpgsqlCommand())
             {
@@ -82,10 +57,70 @@ namespace try2
                 insertCmd.Parameters.AddWithValue("@password", password);
                 insertCmd.Parameters.AddWithValue("@name", name);
                 insertCmd.Parameters.AddWithValue("@location", location);
-                insertCmd.Parameters.AddWithValue("@join_date", DateTime.Now);
+                insertCmd.Parameters.AddWithValue("@join_date", join_date);
 
                 int insertRowsAffected = insertCmd.ExecuteNonQuery();
                 Console.WriteLine($"Rows Inserted: {insertRowsAffected}");
+            }
+        }
+
+        static void UpdateUserName(NpgsqlConnection connection, NpgsqlTransaction transaction, int userIdToUpdate, string newName)
+        {
+            using (NpgsqlCommand updateCmd = new NpgsqlCommand())
+            {
+                updateCmd.Connection = connection;
+                updateCmd.Transaction = transaction;
+
+                updateCmd.CommandText = "UPDATE users SET name = @newName WHERE user_id = @userId";
+                updateCmd.Parameters.AddWithValue("@newName", newName);
+                updateCmd.Parameters.AddWithValue("@userId", userIdToUpdate);
+
+                int updateRowsAffected = updateCmd.ExecuteNonQuery();
+                Console.WriteLine($"Rows Updated: {updateRowsAffected}");
+            }
+        }
+
+        static void DeleteUser(NpgsqlConnection connection, NpgsqlTransaction transaction, int userIdToDelete)
+        {
+            using (NpgsqlCommand deleteCmd = new NpgsqlCommand())
+            {
+                deleteCmd.Connection = connection;
+                deleteCmd.Transaction = transaction;
+
+                deleteCmd.CommandText = "DELETE FROM users WHERE user_id = @userId";
+                deleteCmd.Parameters.AddWithValue("@userId", userIdToDelete);
+
+                int deleteRowsAffected = deleteCmd.ExecuteNonQuery();
+                Console.WriteLine($"Rows Deleted: {deleteRowsAffected}");
+            }
+        }
+
+        static void SelectUser(NpgsqlConnection connection, NpgsqlTransaction transaction, int userId)
+        {
+            using (NpgsqlCommand selectCmd = new NpgsqlCommand())
+            {
+                selectCmd.Connection = connection;
+                selectCmd.Transaction = transaction;
+
+                selectCmd.CommandText = "SELECT user_id, email, name, location FROM users WHERE user_id = @userId";
+                selectCmd.Parameters.AddWithValue("@userId", userId);
+
+                using (NpgsqlDataReader reader = selectCmd.ExecuteReader())
+                {
+                    if (reader.Read())
+                    {
+                        int userIdSelected = reader.GetInt32(0);
+                        string email = reader.GetString(1);
+                        string name = reader.GetString(2);
+                        string location = reader.GetString(3);
+
+                        Console.WriteLine($"Selected User Info - User ID: {userIdSelected}, Email: {email}, Name: {name}, Location: {location}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("User not found.");
+                    }
+                }
             }
         }
     }
